3957cd73088d611880d3adc4762c65d2e70065d3
shourov223 <shourav4316@gmail.com>
2026-03-28 16:22:39 +0600

    Project Complite

--- app/categoryProduct/[category]/page.jsx
+++ app/categoryProduct/[category]/page.jsx
@@ -25,7 +25,7 @@ const CategoryPage = () => {
             No products found in this category.
           </p>
         ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
             {categoryProducts.map((item) => (
               <ProductCard
                 key={item.id}
